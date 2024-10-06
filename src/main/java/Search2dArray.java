36bec2c1c976e7c2ffb6167cf5758fa0fa71213d
Saumya Puthenveettil <sputhen2000@gmail.com>
2024-10-06 15:33:58 +0000

    Completed Coding Challenge: Search 2D Array

--- src/main/java/Search2dArray.java
+++ src/main/java/Search2dArray.java
@@ -10,6 +10,13 @@ public class Search2dArray {
      * @return true if nums contains target, and false otherwise.
      */
     public boolean search(int[][] nums, int target){
+        for (int i=0;i<nums.length;i++){
+            for (int k=0; k<nums[i].length;k++){
+                if (nums[i][k] == target){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
